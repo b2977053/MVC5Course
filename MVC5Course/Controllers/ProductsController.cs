a3cf91f5e068097b30fec1114ec48c1bab964e41
cst-NB <stno2cnyes@gmail.com>
2018-07-07 14:31:19 +0800

    +ValueInjecter 的基本用法

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using Omu.ValueInjecter;
 
 namespace MVC5Course.Controllers
 {
@@ -95,9 +96,10 @@ namespace MVC5Course.Controllers
 
             var one = db.Product.Find(id);
 
-            one.ProductName = data.ProductName;
-            one.Stock = data.Stock;
-            one.Price = data.Price;
+            //one.ProductName = data.ProductName;
+            //one.Stock = data.Stock;
+            //one.Price = data.Price;
+            one.InjectFrom(data);
 
             db.SaveChanges();
 
