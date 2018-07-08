ce08e71bec732cdfd5da7b38f8355c0a0640e15e
cst-NB <stno2cnyes@gmail.com>
2018-07-08 16:08:04 +0800

    +18 練習建立 BaseController

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -6,23 +6,18 @@ using System.Web.Mvc;
 
 namespace MVC5Course.Controllers
 {
-    public class HomeController : BaseController
+    public class ARController : Controller
     {
+        // GET: AR
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult ViewTest()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
-        }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
