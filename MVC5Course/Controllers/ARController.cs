afc67d173480604ef77961f3ea59c225b4157787
cst-NB <stno2cnyes@gmail.com>
2018-07-10 22:02:12 +0800

    +22 練習用 FileResult 輸出一張圖片，並透過 ?dl=1 下載圖片

--- MVC5Course/Controllers/ARController.cs
+++ MVC5Course/Controllers/ARController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -20,5 +21,28 @@ namespace MVC5Course.Controllers
             //return View((object)model);
             return PartialView((object)model);
         }
+
+
+        public ActionResult ContentTest()
+        {
+            return Content("Test Content!",
+                "text/plain",
+                Encoding.GetEncoding("Big5"));
+        }
+
+        public ActionResult FileTest(string dl)
+        {
+            if (String.IsNullOrEmpty(dl))
+            {
+                return File(Server.MapPath("~/App_Data/42834332472_c3582b910b_b.jpg"),
+                    "image/jpeg");
+            }
+            else
+            {
+                return File(Server.MapPath("~/App_Data/42834332472_c3582b910b_b.jpg"),
+                    "image/jpeg", "My-FIFA-Picture.jpg");
+            }
+        }
+
     }
 }
